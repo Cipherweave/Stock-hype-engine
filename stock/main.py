d8964316a9174564f8a4e46d9eba5168aae76cc4
Ali Karimi <119451187+Cipherweave@users.noreply.github.com>
2024-06-01 22:42:07 -0400

    Add files via upload

---
+++ stock/main.py
@@ -0,0 +1,59 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from news import News
+from stocks import Stock
+
+
+class Program:
+    """ A class to represent a program """
+
+    def __init__(self, stock_list: list) -> None:
+        """ Initialize a program
+
+        """
+        finviz = 'https://finviz.com/quote.ashx?t='
+        new_tables = {}
+        for stock in stock_list:
+            url = finviz + stock
+            req = Request(url, headers={'User-Agent': 'me'})
+            response = urlopen(req)
+            html = BeautifulSoup(response, 'html.parser')
+            new_table = html.find(id='news-table')
+            new_tables[stock] = new_table
+
+        # aapl_data = new_tables['AAPL']
+        # aapl_rows = aapl_data.findAll('tr')
+
+        self.stocks = []  # list of Stock objects
+        for stock, news_table in new_tables.items():
+            s = Stock(stock)
+            for row in news_table.findAll('tr'):
+
+                title = row.a.get_text()
+                date = row.td.text.split(" ")
+                link = row.a.get('href')
+
+                if len(date) == 1:
+                    time = date[0]
+                else:
+                    time = date[1]
+                    date = date[0]
+                k = News(title, time, date, link)
+                s.add_news(News(title, time, date, link))
+            self.stocks.append(s)
+
+    def get_best_stock(self) -> Stock:
+        """ Return the stock with the highest hype level
+        """
+        best_stock = self.stocks[0]
+        for stock in self.stocks:
+            if stock.get_stock_hype() > best_stock.get_stock_hype():
+                best_stock = stock
+        return best_stock
+
+
+if __name__ == '__main__':
+    stock_list = ['AAPL']
+    p = Program(stock_list)
+    best_stock = p.get_best_stock()
+    print(best_stock.stock_name, best_stock.get_stock_hype())
---
+++ stock/stocks.py
@@ -0,0 +1,34 @@
+class Stock:
+    """ Class to store stock data """
+
+    def __init__(self, stock_name: str) -> None:
+        """ Initialize a stock object
+        """
+        self.stock_name = stock_name
+        self.news = []
+
+    def __str__(self) -> str:
+        """ Return a string representation
+        """
+        return f"{self.stock_name} {self.news}"
+
+    def add_news(self, news: object) -> None:
+        """ Add news to the news list
+        """
+        self.news.append(news)
+
+    def get_news(self) -> list:
+        """ Return the news list
+        """
+        return self.news
+
+    def get_stock_hype(self) -> float:
+        """ Return the hype level of the stock
+        """
+        total = 0
+        for news in self.news:
+            total += news.get_sentiment()
+        return total / len(self.news)
+
+
+
